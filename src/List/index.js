c1dafd47594c3099f4a01ea2103a866880a1e414
Szymon Uscinski <szymonuscinski@gmail.com>
2021-06-11 14:31:52 +0200

    added key attribute

--- src/List/index.js
+++ src/List/index.js
@@ -4,6 +4,7 @@ const List = ({ tasks, hideDoneTasks }) => (
     <ul className="list">
         {tasks.map(task => (
             <li
+                key={task.id}
                 className={`list__item${
                     task.done && hideDoneTasks 
                     ? " list__item--hidden" 
